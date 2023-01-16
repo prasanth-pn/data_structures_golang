7d06d277b9e9e7e20962953124da40d127c5c318
prasanth-pn <prasanthpn68@gmail.com>
2023-01-16 18:28:59 +0530

    double linked list operations delete and inorder and reverse order is done

--- double_linkedlist/main.go
+++ double_linkedlist/main.go
@@ -1,6 +1,8 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+)
 
 type node struct {
 	data int
@@ -17,9 +19,65 @@ func main() {
 	data := []int{12, 13, 14, 15}
 	//fmt.Println(data)
 	list.addnode(data)
+
+	//list.search(12)
+	list.insert(120, 15)
+	list.insert(200, 14)
 	list.display()
+	fmt.Println("\n\n\n the reverse order is \n ")
+	list.reverseOrder()
+	fmt.Println("the ordered values are")
+	list.order()
+
+}
+func (c *double) order() {
+	temp := c.head
+	for temp != nil {
+		fmt.Println(temp.data)
+		temp = temp.next
+	}
 }
 
+func (c *double) reverseOrder() {
+	temp := c.tail
+
+	for temp != nil {
+		fmt.Println(temp.data)
+		temp = temp.pre
+	}
+
+}
+func (c *double) insert(value int, search int) {
+	newnode := new(node)
+	newnode.data = value
+	newnode.pre = nil
+	newnode.next = nil
+	temp := c.head
+	if temp.data == search {
+		temp.pre = newnode
+		newnode.next = temp
+		c.head = newnode
+		fmt.Println("node added successfully")
+		return
+	}
+
+	for temp.data != search {
+		
+		if temp.next.data == search {
+			newnode.next = temp.next
+			newnode.pre = temp
+			temp.next.pre = newnode
+			temp.next = newnode
+
+			break
+		}
+
+		temp = temp.next
+	}
+
+}
+
+// ----------------------------add node
 func (c *double) addnode(data []int) {
 	for _, v := range data {
 
@@ -30,20 +88,43 @@ func (c *double) addnode(data []int) {
 		if c.head == nil {
 			c.head = newnode
 			c.tail = newnode
-		
 
 		} else {
-			c.tail.pre = c.tail
+			newnode.pre = c.tail
+
 			c.tail.next = newnode
 
 		}
 		c.tail = newnode
 	}
 }
+
+// --------------------------------display values
 func (c *double) display() {
 	temp := c.head
 	for temp != nil {
 		fmt.Println(temp.data)
 		temp = temp.next
 	}
+}
+
+// -------------------------------insert node-----
+func (c *double) search(k int) {
+	temp := c.head
+
+	if temp.data == k {
+		fmt.Println("the data is available in first node")
+		return
+	}
+	for temp.next != nil {
+		if temp.next.data == k {
+			fmt.Println("the data is available")
+
+			return
+		}
+		temp = temp.next
+	}
+
+	fmt.Println("the data is not available")
+
 }
