d211a8c4bd1747cb633da5e5bb0adca9fe1504e2
prasanth-pn <prasanthpn68@gmail.com>
2023-01-16 16:45:52 +0530

    singly liniked list data insertion is completed

--- siglylinkedlist/main.go
+++ siglylinkedlist/main.go
@@ -18,20 +18,64 @@ func main() {
 
 	data := []int{12, 13, 14, 15, 16}
 	list.Addnode(data)
-	list.display()
-	list.delete(12)
+
+	//list.delete(13)
+
+	//list.display()
+	//list.delete(12)
+	//list.delete(15)
+	//list.delete(16)
+
+	list.insert(150, 16)
 	list.display()
 
 }
+// search the value and add new node
+func (c *single) insert(data int, search int) {
+	newnode := new(node)
+	newnode.data = data
+	temp := c.head
+	if search == temp.data {
+		temp.data = data
+		return
+
+	}
+	for temp.next != nil {
+		if temp.next.data == search {
+			newnode.next = temp.next
+			temp.next = newnode
+			return
+
+		}
+		temp = temp.next
+	}
+	fmt.Println("the number is not available")
+}
+//---------------------------delete the node 
 func (list *single) delete(data int) {
 	temp := list.head
 	if temp.data == data {
 		list.head = temp.next
-		fmt.Println("\n\n\n\n ")
+		return
+	} else {
+		temp = list.head
+		for temp != nil {
+			if temp.next.data == data {
+				if temp.next == list.tail {
+					list.tail = temp
+					list.tail.next = nil
+
+				} else {
+					temp.next = temp.next.next
+					return
+				}
+			}
+			temp = temp.next
+		}
 	}
-	return
 
 }
+//----------------------------- add the node    
 
 func (list *single) Addnode(data []int) {
 	for _, v := range data {
@@ -51,6 +95,7 @@ func (list *single) Addnode(data []int) {
 	fmt.Println("\n\n\n\n ")
 
 }
+//-------------------------------------display the values
 func (list *single) display() {
 	temp := list.head
 	if list.head == nil {
